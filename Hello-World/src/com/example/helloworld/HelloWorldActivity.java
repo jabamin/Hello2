4fd599a7e9b65a2045902290a4774c6213d22941
jabamin <jabamin2013@gmail.com>
2012-01-21 15:06:14 +0100

    bureau test

--- Hello-World/src/com/example/helloworld/HelloWorldActivity.java
+++ Hello-World/src/com/example/helloworld/HelloWorldActivity.java
@@ -2,7 +2,6 @@ package com.example.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class HelloWorldActivity extends Activity {
     /** Called when the activity is first created. */
